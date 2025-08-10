8162072208d049c0c0f1129d61d24eba85e10f78
Ivan V <ivv@i1v.org>
2025-08-11 01:10:29 +0300

    utils/loader: improve HDD handling

--- utils/loader/loader.c
+++ utils/loader/loader.c
@@ -7,7 +7,7 @@
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
 #
-# Modified to not reset IOP if not explicitly requested
+# Modified to handle HDD devices and to not reset IOP if not explicitly requested
 */
 
 #include <iopcontrol.h>
@@ -16,6 +16,11 @@
 #include <ps2sdkapi.h>
 #include <sifrpc.h>
 #include <string.h>
+
+#define NEWLIB_PORT_AWARE
+#include <fileXio_rpc.h>
+#include <hdd-ioctl.h>
+#include <io_common.h>
 
 //--------------------------------------------------------------
 // Redefinition of init/deinit libc:
@@ -47,6 +52,41 @@ static void wipeUserMem(void) {
   }
 }
 
+// Mounts the partition specified in path
+int mountPFS(char *path) {
+  // Extract partition path
+  char *filePath = strstr(path, ":pfs:");
+  char pathSeparator = '\0';
+  if (filePath || (filePath = strchr(path, '/'))) {
+    // Terminate the partition path
+    pathSeparator = filePath[0];
+    filePath[0] = '\0';
+  }
+
+  // Mount the partition
+  int res = fileXioMount("pfs0:", path, FIO_MT_RDONLY);
+  if (pathSeparator != '\0')
+    filePath[0] = pathSeparator; // Restore the path
+  if (res)
+    return -ENODEV;
+
+  return 0;
+}
+
+// Puts HDD in idle mode and powers off the dev9 device
+void shutdownDEV9() {
+  // Unmount the partition (if mounted)
+  fileXioUmount("pfs0:");
+  // Immediately put HDDs into idle mode
+  fileXioDevctl("hdd0:", HDIOC_IDLEIMM, NULL, 0, NULL, 0);
+  fileXioDevctl("hdd1:", HDIOC_IDLEIMM, NULL, 0, NULL, 0);
+
+  // Turn off dev9
+  fileXioDevctl("dev9x:", DDIOC_OFF, NULL, 0, NULL, 0);
+}
+
+// Loads ELF from file specified in argv[0]
+// Will reset IOP if argv[0] is "-r"
 int main(int argc, char *argv[]) {
   static t_ExecData elfdata;
   int ret;
@@ -61,15 +101,20 @@ int main(int argc, char *argv[]) {
   int doIOPReset = 0;
   if (!strcmp(argv[0], "-r")) {
     argv++;
+    argc--;
     doIOPReset = 1;
   }
 
-  // Initialize
-  sceSifInitRpc(0);
-  wipeUserMem();
-
   char *elfPath = NULL;
+
+  // Init SIF RPC
+  sceSifInitRpc(0);
+
   if (!strncmp(argv[0], "hdd", 3)) {
+    // Mount the partition
+    if (mountPFS(argv[0]))
+      return -ENODEV;
+
     // HDD paths usually look as follows: hdd0:<partition name>:pfs:/<path to ELF>
     // However, SifLoadElf needs PFS path, not hdd0:
     // Extract PFS path from the argument
@@ -81,21 +126,30 @@ int main(int argc, char *argv[]) {
   } else
     elfPath = argv[0];
 
-  // Writeback data cache before loading ELF.
+  wipeUserMem();
+  // Writeback data cache before loading the ELF
   FlushCache(0);
 
+  // Load ELF
   SifLoadFileInit();
   ret = SifLoadElf(elfPath, &elfdata);
   if (ret) // Try to load KELF if SifLoadElf fails
-    ret = SifLoadElfEncrypted(elfPath, &elfdata);
+    if ((ret = SifLoadElfEncrypted(elfPath, &elfdata)))
+      return ret;
   SifLoadFileExit();
+
+  // Shutdown DEV9
+  if (!strncmp(argv[0], "hdd", 3))
+    shutdownDEV9();
 
   if (doIOPReset) {
     while (!SifIopReset("", 0)) {
     };
     while (!SifIopSync()) {
     };
-  }
+    sceSifInitRpc(0);
+  } else
+    sceSifExitRpc();
 
   if (ret == 0 && elfdata.epc != 0) {
     FlushCache(0);
