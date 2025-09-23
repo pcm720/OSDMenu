454b077e84deb31881ed00116bfd6d609999a0ed
Ivan V <ivv@i1v.org>
2025-09-24 02:29:27 +0300

    mbr: add missing headers

--- mbr/include/disc.h
+++ mbr/include/disc.h
@@ -4,6 +4,9 @@
 // Starts PS1/PS2 game CD/DVD
 int startGameDisc();
 
+// Boots PS2 disc directly or via PS2LOGO
+void handlePS2Disc(char *bootPath);
+
 // Starts the DVD Player from a memory card or ROM
 int startDVDVideo();
 
--- mbr/include/hdd.h
+++ mbr/include/hdd.h
@@ -1,6 +1,8 @@
 #ifndef _HDD_H_
 #define _HDD_H_
 
+// Functions implemented in hdd.c
+
 // Puts HDD in idle mode and powers off the dev9 device
 void shutdownDEV9();
 
@@ -18,5 +20,13 @@ int mountPFS(char *path);
 
 // Unmounts the currently mounted partition
 int umountPFS();
+
+// Functions implemented in hdd_start.c
+
+// Starts HDD application using data from the partition attribute area header
+int startHDDApplication(int argc, char *argv[]);
+
+// Starts the dnasload applcation
+void startDNAS(int argc, char *argv[]);
 
 #endif
