50a82bf4861df0736d31ea285439b2b47f77a43a
Ivan V <ivv@i1v.org>
2025-10-06 03:17:56 +0300

    utils/installer: update for changes in project structure

--- utils/installer/src/main.c
+++ utils/installer/src/main.c
@@ -30,19 +30,19 @@ int main(int argc, char *argv[]) {
   init_scr();
   scr_setCursor(0);
   scr_clear();
-  scr_printf(".\nHOSDMenu MBR installer\n\n\n\n\nInitializing modules...\n\n\n");
+  scr_printf(".\n\n\n\tOSDMenu MBR installer\n\n\n\tInitializing modules...\n\n\n");
   int res = initModules();
   if (res < 0) {
-    scr_printf("Failed to initialize modules: %d", res);
+    scr_printf("\tFailed to initialize modules: %d", res);
     goto done;
   }
 
-  scr_printf("Injecting the payload into hdd0:__mbr:\n");
+  scr_printf("\tInjecting the payload into hdd0:__mbr:\n");
   res = writeMBR(payload_bin, size_payload_bin);
   if (res < 0)
-    scr_printf("\n\nFailed: %d\n", res);
+    scr_printf("\n\n\tFailed: %d\n", res);
   else
-    scr_printf("\n\nDone\n");
+    scr_printf("\n\n\tDone\n");
 
   fileXioDevctl("dev9x:", DDIOC_OFF, NULL, 0, NULL, 0);
 
@@ -57,11 +57,11 @@ int writeMBR(char *payload, int size) {
   // Buffer for read/write operations
   uint8_t buf[512 + sizeof(hddAtaTransfer_t)] __attribute__((aligned(64)));
 
-  scr_printf("Retrieving __mbr start sector\n");
+  scr_printf("\tRetrieving __mbr start sector\n");
   iox_stat_t mbrStat;
   int res = fileXioGetStat("hdd0:__mbr", &mbrStat);
   if (res < 0) {
-    scr_printf("Failed to get __mbr stats: %d\n", res);
+    scr_printf("\tFailed to get __mbr stats: %d\n", res);
     return res;
   }
 
@@ -70,7 +70,7 @@ int writeMBR(char *payload, int size) {
   uint32_t sectorCount = (size / 512) + ((remainder) ? 1 : 0);
   uint32_t byteCount = 512;
 
-  scr_printf("Writing the payload at sector %ld\n", startSector);
+  scr_printf("\tWriting the payload at sector %ld\n", startSector);
   for (int i = 0; i < sectorCount; i++) {
     // If this the last sector and the payload doesn't occupy it fully
     if ((i == (sectorCount - 1)) && (remainder != 0)) {
@@ -79,7 +79,7 @@ int writeMBR(char *payload, int size) {
       ((hddAtaTransfer_t *)buf)->lba = startSector + i;
       ((hddAtaTransfer_t *)buf)->size = 1;
       if ((res = fileXioDevctl("hdd0:", HDIOC_READSECTOR, buf, sizeof(hddAtaTransfer_t), buf + sizeof(hddAtaTransfer_t), 512)) < 0) {
-        scr_printf("\nFailed to read sector %d/%ld: %d\n", i + 1, sectorCount, res);
+        scr_printf("\n\tFailed to read sector %d/%ld: %d\n", i + 1, sectorCount, res);
         return res;
       }
     }
@@ -87,24 +87,24 @@ int writeMBR(char *payload, int size) {
     // Fill the buffer with payload data
     memcpy(buf + sizeof(hddAtaTransfer_t), payload + 512 * i, byteCount);
 
-    scr_printf("\rWriting sector %d/%ld", i + 1, sectorCount);
+    scr_printf("\r\tWriting sector %d/%ld", i + 1, sectorCount);
     ((hddAtaTransfer_t *)buf)->lba = startSector + i;
     ((hddAtaTransfer_t *)buf)->size = 1;
     if ((res = fileXioDevctl("hdd0:", HDIOC_WRITESECTOR, buf, 512 + sizeof(hddAtaTransfer_t), NULL, 0)) < 0) {
-      scr_printf("\nFailed to write sector %d/%ld: %d\n", i + 1, sectorCount, res);
+      scr_printf("\n\tFailed to write sector %d/%ld: %d\n", i + 1, sectorCount, res);
       return res;
     }
   }
 
   // Set MBR start and size
-  scr_printf("\nUpdating OSD MBR data\n");
+  scr_printf("\n\tUpdating OSD MBR data\n");
   if (res >= 0) {
     hddSetOsdMBR_t mbr = {
         .start = startSector,
         .size = sectorCount,
     };
     if ((res = fileXioDevctl("hdd0:", HDIOC_SETOSDMBR, &mbr, sizeof(mbr), NULL, 0)))
-      scr_printf("Failed to set MBR: %d\n", res);
+      scr_printf("\tFailed to set MBR: %d\n", res);
   }
   return res;
 }
